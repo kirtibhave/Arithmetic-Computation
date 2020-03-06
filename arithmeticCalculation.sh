bb2b0ea28be3a38fb253ce4f38131a578402747d
kirtibhave <kirtibhave1000@gmail.com>
2020-03-06 16:56:14 +0530

    ADDED:Stored results in dictionary for every computation

--- arithmeticCalculation.sh
+++ arithmeticCalculation.sh
@@ -1,16 +1,33 @@
 #!/bin/bash -x
 echo "**********Welcome to arithmetic computation & sorting**********"
 
+declare -A DictResult
 read -p  "enter first number" number1
 read -p  "enter second  number" number2
 read -p  "enter third  number" number3
 
-firstOpeartionResult=`echo "scale=2; $number1+$number2*$number3" | bc`
-secondOperationResult=`echo "scale=2; $number1*$number2+$number3" | bc`
-thirdOperationResult=`echo "scale=2; $number3+$number1/$number2" | bc`
-fourthOperationResult=`echo "scale=2; $number1%$number2+$number3" | bc`
+result1=`echo "scale=2; $number1+$number2*$number3" | bc`
+result2=`echo "scale=2; $number1*$number2+$number3" | bc`
+result3=`echo "scale=2; $number3+$number1/$number2" | bc`
+result4=$( echo "scale=2; $number1%$number2+$number3 " | bc ) 
 
-echo "Result of first arithmetic operation is: $firstOpeartionResult"
-echo "Result of second arithmetic opeartion is: $secondOperationResult"
-echo "Result of third arithmetic opeartion is: $thirdOperationResult"
-echo "Result of fourth arithmetic opeartion is: $fourthOperationResult"
+echo "Result of first arithmetic operation is: $result1"
+echo "Result of second arithmetic opeartion is: $result2"
+echo "Result of third arithmetic opeartion is: $result3"
+echo "Result of fourth arithmetic opeartion is: $result4"
+
+DictResult[first]="$result1"
+DictResult[second]="$result2"
+DictResult[three]="$result3"
+DictResult[fourth]="$result4"
+
+echo ${DictResult[@]}
+echo ${!DictResult[@]}
+
+
+
+
+
+
+
+
