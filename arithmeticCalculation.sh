f025d2acacc318f351421a0238b45e599b7531e8
kirtibhave <kirtibhave1000@gmail.com>
2020-03-06 11:28:52 +0530

    ADDED:Computed first arithmetic operation

--- arithmeticCalculation.sh
+++ arithmeticCalculation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 echo "**********Welcome to arithmetic computation & sorting**********"
 
-read -p  "enter first number" a
-read -p  "enter second  number" b
-read -p  "enter third  number" c
+read -p  "enter first number" number1
+read -p  "enter second  number" number2
+read -p  "enter third  number" number3
+
+result=`echo "scale=2; $number1+$number2*$number3" | bc`
+echo "Result is: $result"
