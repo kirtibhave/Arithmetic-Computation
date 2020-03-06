b54151350d33245db88fd78e46605ec51dcad62b
kirtibhave <kirtibhave1000@gmail.com>
2020-03-06 10:36:08 +0530

    ADDED:Variables

--- arithmeticCalculation.sh
+++ arithmeticCalculation.sh
@@ -1,1 +1,6 @@
+#!/bin/bash -x
 echo "**********Welcome to arithmetic computation & sorting**********"
+
+read -p  "enter first number" a
+read -p  "enter second  number" b
+read -p  "enter third  number" c
